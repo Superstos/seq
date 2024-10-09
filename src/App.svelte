b8914ad1d72aa390c279584643ff9c1138aa61b5
Mohamed Amine NAJI <122235965+Superstos@users.noreply.github.com>
2024-10-09 16:21:45 +0100

    ui sync fixes

--- src/App.svelte
+++ src/App.svelte
@@ -7,6 +7,7 @@
   KICKSAMPLE.connect(volumeControl);
   
   let bpm = 600; // Initial BPM
+  let kmh = (bpm * 60) / 1000;
   let beat = 0;
   let isPlaying = false;
 
@@ -52,19 +53,20 @@
   $: if (isPlaying) {
       Tone.Transport.bpm.value = bpm; // Update BPM while playing
   }
+  $: kmh = (bpm * 60) / 1000;
 </script>
 
 <main class="subpixel-antialiased min-h-screen h-full dark:bg-zinc-800 bg-zinc-200">
   <div class="flex flex-col max-w-3xl mx-auto p-3 text-zinc-900 dark:text-zinc-100 text-balance">
-    <p class="max-w-xl mx-auto font-normal text-center text-lg p-3 dark:text-red-600 text-red-700">
-      <strong class="inline-flex items-center gap-1 text-xl font-semibold">
+    <p class="max-w-xl mx-auto font-medium text-center text-lg p-3 dark:text-red-600 text-red-700">
+      <strong class="inline-flex items-center gap-1 text-3xl font-semibold">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
           <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" />
         </svg>        
         Warning: 
       </strong><br>if your machine is not of today's standard don't push the slider more than 5000
       and even if you do be careful near 8000. <br>
-      <span class="font-light">if you do just try to lower the bpm and press stop if it crashes it's ok, <br>
+      <span class="font-normal">if you do just try to lower the bpm and press stop if it crashes it's ok, <br>
         you may open a new tab
       </span>
     </p>
@@ -74,15 +76,18 @@
       a non perfect fake sounding car engine will be produced.
     </p>
     <i class="text-base text-center border-l-4 border-zinc-500 p-3 my-3 bg-zinc-300 dark:bg-zinc-700">
-      note: sorry for the shubby explanation. but if you know what you are doing; 
+      note:
+      the calculations may seem wrong most of the times <br>
+      sorry for the shubby explanation.
+      but if you know what you are doing; 
       either way have fun.
     </i>
   </div>
   <div id="controls" class="grid grid-cols-1 gap-3 justify-between max-w-xl mx-auto p-3 text-zinc-900 dark:text-zinc-100">
     <div class="grid grid-cols-1 md:grid-cols-2 gap-3">
       <div class="inline-flex flex-wrap items-center gap-2">
-        <label class="w-32" for="bpm">{bpm} BPM</label>
-        <input type="range" id="bpm" name="bpm" min="100" bind:value={bpm} max="15000" step="10" 
+        <label class="w-52" for="bpm">{bpm} BPM | {kmh.toFixed(2)} km/h</label>
+        <input type="range" id="bpm" name="bpm" min="100" bind:value={bpm} max="10000" step="10" 
         class="w-full bg-transparent cursor-pointer appearance-none disabled:opacity-50 disabled:pointer-events-none focus:outline-none
         [&::-webkit-slider-thumb]:w-2.5
         [&::-webkit-slider-thumb]:h-2.5
@@ -119,7 +124,7 @@
         [&::-moz-range-track]:rounded-full">
       </div>
       <div class="inline-flex flex-wrap items-center gap-2">
-        <label class="" for="volume">Volume</label>
+        <label class="w-52" for="volume">Volume</label>
         <input type="range" id="volume" name="volume" min="-1" max="0" step="0.01" on:input={handleVolumeChange} 
         class="w-full bg-transparent cursor-pointer appearance-none disabled:opacity-50 disabled:pointer-events-none focus:outline-none
         [&::-webkit-slider-thumb]:w-2.5
@@ -181,8 +186,8 @@
       {#each rows as row, i}
         {#each row as note, j}
           <button class="w-16 h-16 squircle 
-            {note.active ? 'bg-red-600' : 'bg-zinc-600'}
-            {j % 4 === 0 ? 'bg-zinc-700' : ''}"
+            {note.active ? 'bg-red-600' : 
+            j % 4 === 0 ? 'bg-zinc-700' : 'bg-zinc-600'}"
             on:click={() => {handleNoteClick(i, j)}}  
           >
           </button>
